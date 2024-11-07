187b2eab85a112764d13a3847bfdac53b0f6e6f5
saraxixi <saraxi.xixi@gmail.com>
2024-11-06 22:13:03 -0800

    Implement navigation with tab and stack navigators for Home, History, Favorite, and Cart screens

--- App.js
+++ App.js
@@ -1,12 +1,59 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Cart from './screen/Cart';
+import Favorite from './screen/Favorite';
+import History from './screen/History';
+import Home from './screen/Home';
+import { NavigationContainer, ThemeProvider } from '@react-navigation/native';
+
+const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
 export default function App() {
+  function HomeStack() {
+    return (
+      <Stack.Navigator screenOptions={{headerShown: false}}>
+        <Stack.Screen name="HomeScreen" component={Home} />
+      </Stack.Navigator>
+    );
+  }
+
+  function HistoryStack() {
+    return (
+      <Stack.Navigator screenOptions={{headerShown: false}}>
+        <Stack.Screen name="HistoryScreen" component={History} />
+      </Stack.Navigator>
+    );
+  }
+
+  function FavoriteStack() {
+    return (
+      <Stack.Navigator screenOptions={{headerShown: false}}>
+        <Stack.Screen name="FavoriteScreen" component={Favorite} />
+      </Stack.Navigator>
+    );
+  }
+
+  function CartStack() {
+    return (
+      <Stack.Navigator screenOptions={{headerShown: false}}>
+        <Stack.Screen name="CartScreen" component={Cart} />
+      </Stack.Navigator>
+    );
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeStack} />
+        <Tab.Screen name="History" component={HistoryStack} />
+        <Tab.Screen name="Favorite" component={FavoriteStack} />
+        <Tab.Screen name="Cart" component={CartStack} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
